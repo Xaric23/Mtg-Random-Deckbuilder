e995f006ab09d4ac494fdb095d00b844f461ac42
Xaric23 <Xaric23@users.noreply.github.com>
2025-11-08 00:25:45 +0000

    deploy: 17abae1c87386a600af83320b940ec277e5a861f

---
+++ _next/static/wJiiU4U2n-wnesxy2je6B/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Mtg-Random-Deckbuilder//_next/:path+",
+        "destination": "/Mtg-Random-Deckbuilder/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
